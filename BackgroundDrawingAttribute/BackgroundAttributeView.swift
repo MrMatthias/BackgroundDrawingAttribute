3ac51c2306674cf6893cb0b67fba5c14ef554aaf
Daniel Bocksteger <daniel.bocksteger@googlemail.com>
2017-04-13 15:45:49 +0200

    Added Swift 3 support

--- BackgroundDrawingAttribute/BackgroundAttributeView.swift
+++ BackgroundDrawingAttribute/BackgroundAttributeView.swift
@@ -5,61 +5,78 @@ import CoreText
 class BackgroundAttributeView: UIView {
     
     var text:NSAttributedString = {
-        var att = [NSFontAttributeName:UIFont(name:"Helvetica", size:20)!]
-
-        var text = NSMutableAttributedString(string: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ullamco", attributes:att)
-        text.appendAttributedString(NSAttributedString(string: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", attributes: ["kBackgroundAttribute":UIColor.cyanColor()]))
-        text.appendAttributedString(NSAttributedString(string: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation", attributes:att))
-        return text
+        
+        if let font = UIFont(name:"Helvetica", size:20) {
+            var att = [NSFontAttributeName:font]
+            
+            var text = NSMutableAttributedString(string: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ullamco ", attributes:att)
+            text.append(NSAttributedString(string: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", attributes: ["kBackgroundAttribute":UIColor.cyan, NSFontAttributeName: font]))
+            text.append(NSAttributedString(string: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation", attributes:att))
+            return text
+        }
+        return NSAttributedString()
     }()
     
-    override func drawRect(dirtyRect: CGRect) {
-        super.drawRect(dirtyRect)
-        var frameSetter = CTFramesetterCreateWithAttributedString(text)
-        var framePath = CGPathCreateMutable()
-        CGPathAddRect(framePath, nil, self.bounds)
-        var ctFrame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), framePath, nil)
-        var frameRange = CTFrameGetVisibleStringRange(ctFrame)
-        var ctx = UIGraphicsGetCurrentContext()
-        CGContextSaveGState(ctx)
+    override func draw(_ dirtyRect: CGRect) {
+        super.draw(dirtyRect)
         
-        CGContextSetTextMatrix(ctx, CGAffineTransformIdentity)
-        CGContextTranslateCTM(ctx, 0, bounds.height)
-        CGContextScaleCTM(ctx, 1.0, -1.0)
-    
+        let frameSetter = CTFramesetterCreateWithAttributedString(text)
+        let framePath = CGMutablePath()
         
-        var lines:NSArray = CTFrameGetLines(ctFrame)
-        var lineOrigins:[CGPoint] = [CGPoint](count:lines.count, repeatedValue:CGPointZero)
-        CTFrameGetLineOrigins(ctFrame, CFRangeMake(0, 0), &lineOrigins)
-        var currentLineHeight:CGFloat = 0
-        var lineHeight:CGFloat = 0
-        for (lineIndex, line) in enumerate(lines) {
-            let lineOrigin = lineOrigins[lineIndex]
+        framePath.addRect(self.bounds)
+        
+        let ctFrame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(0, 0), framePath, nil)
+        
+        _ = CTFrameGetVisibleStringRange(ctFrame)
+        
+        if let  ctx = UIGraphicsGetCurrentContext() {
+            ctx.saveGState()
+            ctx.textMatrix = CGAffineTransform.identity
+            ctx.translateBy(x: 0, y: bounds.height)
+            ctx.scaleBy(x: 1.0, y: -1.0)
             
-            var runs:NSArray = CTLineGetGlyphRuns(line as! CTLine)
-            for run:CTRunRef in runs as! [CTRunRef] {
-                let stringRange = CTRunGetStringRange(run)
-                var ascent:CGFloat = 0
-                var descent:CGFloat = 0
-                var leading:CGFloat = 0
-                let typographicBounds = CTRunGetTypographicBounds(run, CFRangeMake(0, 0), &ascent, &descent, &leading)
-                var xOffset:CGFloat = CTLineGetOffsetForStringIndex(line as! CTLine, stringRange.location, nil)
-                CGContextSetTextPosition(ctx, lineOrigin.x ,lineOrigin.y + descent )
-                var currentLineHeight = ascent + descent + leading
-                if currentLineHeight > lineHeight {
-                    lineHeight = currentLineHeight
+            if let lines = CTFrameGetLines(ctFrame) as? [CTLine] {
+                var lineOrigins:[CGPoint] = [CGPoint](repeating: CGPoint.zero, count: lines.count)
+                CTFrameGetLineOrigins(ctFrame, CFRangeMake(0, 0), &lineOrigins)
+                var _:CGFloat = 0
+                var lineHeight:CGFloat = 0
+                
+                for (lineIndex, line) in lines.enumerated() {
+                    let lineOrigin = lineOrigins[lineIndex]
+                    
+                    let runs:NSArray = CTLineGetGlyphRuns(line)
+                    for run:CTRun in runs as! [CTRun] {
+                        let stringRange = CTRunGetStringRange(run)
+                        var ascent:CGFloat = 0
+                        var descent:CGFloat = 0
+                        var leading:CGFloat = 0
+                        let typographicBounds = CTRunGetTypographicBounds(run, CFRangeMake(0, 0), &ascent, &descent, &leading)
+                        let xOffset:CGFloat = CTLineGetOffsetForStringIndex(line, stringRange.location, nil)
+                        
+                        ctx.textPosition = CGPoint(x: lineOrigin.x, y: lineOrigin.y + descent)
+                        
+                        let currentLineHeight = ascent + descent + leading
+                        
+                        if currentLineHeight > lineHeight {
+                            lineHeight = currentLineHeight
+                        }
+                        let runBounds = CGRect(x: lineOrigin.x + xOffset, y: lineOrigin.y, width: CGFloat(typographicBounds), height: ascent + descent)
+                        let attributes:NSDictionary = CTRunGetAttributes(run)
+                        let maybeColor = attributes.value(forKey: "kBackgroundAttribute") as! UIColor?
+                        if let color = maybeColor {
+                            let path = UIBezierPath(roundedRect: runBounds, cornerRadius: 3)
+                            color.setFill()
+                            path.fill()
+                        }
+                        CTRunDraw(run, ctx, CFRangeMake(0, 0))
+                    }
                 }
-                var runBounds = CGRectMake(lineOrigin.x + xOffset, lineOrigin.y, CGFloat(typographicBounds), ascent + descent)
-                var attributes:NSDictionary = CTRunGetAttributes(run)
-                var maybeColor = attributes.valueForKey("kBackgroundAttribute") as! UIColor?
-                if let color = maybeColor {
-                    let path = UIBezierPath(roundedRect: runBounds, cornerRadius: 3)
-                    color.setFill()
-                    path.fill()
-                }
-                CTRunDraw(run, ctx, CFRangeMake(0, 0))
+                ctx.restoreGState()
             }
         }
-        CGContextRestoreGState(ctx)
+    }
+    
+    override func layoutSubviews() {
+        self.draw(self.bounds)
     }
 }
